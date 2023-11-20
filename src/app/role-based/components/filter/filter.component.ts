12e08c1244713be4dfb62c769640e3087379a4a1
vitun bajwa <vitunbajwawins@gmail.com>
2023-11-20 09:45:39 +0530

    filter-example

--- src/app/role-based/components/filter/filter.component.ts
+++ src/app/role-based/components/filter/filter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Rating } from '@syncfusion/ej2-angular-inputs';
 import { log } from 'console';
 
 @Component({
@@ -41,7 +42,7 @@ export class FilterComponent implements OnInit {
     console.log(xForce);
   }
 
-  filterOne(){
+  filterOne() {
     const characters = [
       { name: 'James T. Kirk', series: ['Star Trek'] },
       { name: 'Spock', series: ['Star Trek', 'Star Trek: The Next Generation'] },
@@ -55,183 +56,517 @@ export class FilterComponent implements OnInit {
     console.log(tngCharacters);
   }
 
-  filterSecond(){
+  filterSecond() {
     let cities = [
-      {name: 'Los Angeles', population: 3792621},
-      {name: 'New York', population: 8175133},
-      {name: 'Chicago', population: 2695598},
-      {name: 'Houston', population: 2099451},
-      {name: 'Philadelphia', population: 1526006}
-  ];
-  let bigCities = cities.filter((city:any) => {
-    return city.population > 3000000;
-  });
-  console.log(bigCities);
+      { name: 'Los Angeles', population: 3792621 },
+      { name: 'New York', population: 8175133 },
+      { name: 'Chicago', population: 2695598 },
+      { name: 'Houston', population: 2099451 },
+      { name: 'Philadelphia', population: 1526006 }
+    ];
+    let bigCities = cities.filter((city: any) => {
+      return city.population > 3000000;
+    });
+    console.log(bigCities);
   }
 
-  filterThird(){
+  filterThird() {
     let creatures = [
-      {name: "Shark", habitat: "Ocean"},
-      {name: "Whale", habitat: "Ocean"},
-      {name: "Lion", habitat: "Savanna"},
-      {name: "Monkey", habitat: "Jungle"}
+      { name: "Shark", habitat: "Ocean" },
+      { name: "Whale", habitat: "Ocean" },
+      { name: "Lion", habitat: "Savanna" },
+      { name: "Monkey", habitat: "Jungle" }
     ];
-    let bigCreatures = creatures.filter((creature:any) => {
+    let bigCreatures = creatures.filter((creature: any) => {
       return creature.habitat === "Ocean";
     });
     console.log(bigCreatures);
   }
 
-  filterFour(){
+  filterFour() {
     let files = [
       { id: 1, name: 'html.md', size: 499 },
-      { id: 2, name: 'css.md', size: 612  },
+      { id: 2, name: 'css.md', size: 612 },
       { id: 3, name: 'javascript.md', size: 1236 }
     ];
 
-    const bigFiles = files.filter((file:any) => {
+    const bigFiles = files.filter((file: any) => {
       return file.size > 1000;
     });
     console.log(bigFiles);
   }
-  arr: any =[]
-  filterFive(){
-  let data= [{
-    id: 10,
-    title: "HP Pavilion 15-DK1056WM",
-    "description": "HP Pavilion 15-DK1056WM Gaming...",
-    "price":[{"ex":1099}] ,
-    "discountPercentage": 6.18,
-    "rating": 4.43,
-    "stock": 89
-  },{
-    "id": 10,
-    "title": "HP Pavilion 15-DK1056WM",
-    "description": "HP Pavilion 15-DK1056WM Gaming...",
-    "price":[{"ex":1093}] ,
-    "discountPercentage": 6.18,
-    "rating": 4.43,
-    "stock": 89
-  },{
-    "id": 10,
-    "title": "HP Pavilion 15-DK1056WM",
-    "description": "HP Pavilion 15-DK1056WM Gaming...",
-    "price":[{"ex":10959}] ,
-    "discountPercentage": 6.18,
-    "rating": 4.43,
-    "stock": 89
-  },{
-    "id": 10,
-    "title": "HP Pavilion 15-DK1056WM",
-    "description": "HP Pavilion 15-DK1056WM Gaming...",
-    "price":[{"ex":1345}] ,
-    "discountPercentage": 6.18,
-    "rating": 4.43,
-    "stock": 89
-  },{
-    "id": 10,
-    "title": "HP Pavilion 15-DK1056WM",
-    "description": "HP Pavilion 15-DK1056WM Gaming...",
-    "price":[{"ex":15645}] ,
-    "discountPercentage": 6.18,
-    "rating": 4.43,
-    "stock": 89
-  },{
-    "id": 10,
-    "title": "HP Pavilion 15-DK1056WM",
-    "description": "HP Pavilion 15-DK1056WM Gaming...",
-    "price":[{"ex":1434}] ,
-    "discountPercentage": 6.18,
-    "rating": 4.43,
-    "stock": 89
-  },{
-    "id": 10,
-    "title": "HP Pavilion 15-DK1056WM",
-    "description": "HP Pavilion 15-DK1056WM Gaming...",
-    "price":[{"ex":1453}] ,
-    "discountPercentage": 6.18,
-    "rating": 4.43,
-    "stock": 89
-  },{
-    "id": 10,
-    "title": "HP Pavilion 15-DK1056WM",
-    "description": "HP Pavilion 15-DK1056WM Gaming...",
-    "price": [{"ex":1065}, {"ex":34534}] ,
-    "discountPercentage": 6.18,
-    "rating": 4.43,
-    "stock": 89
-  },{
-    "id": 10,
-    "title": "HP Pavilion 15-DK1056WM",
-    "description": "HP Pavilion 15-DK1056WM Gaming...",
-    "price":[{"ex":1076}] ,
-    "discountPercentage": 6.18,
-    "rating": 4.43,
-    "stock": 89
-  },{
-    "id": 10,
-    "title": "HP Pavilion 15-DK1056WM",
-    "description": "HP Pavilion 15-DK1056WM Gaming...",
-    "price":[{"ex":1011}] ,
-    "discountPercentage": 6.18,
-    "rating": 4.43,
-    "stock": 89
-  }]
-  let totalSum: number = 0
-  const  x = data.forEach((item:any) => (item.price.forEach((prices: any) => totalSum = totalSum + prices.ex) ))
-  console.log(totalSum);
+  arr: any = []
+  filterFive() {
+    let data = [{
+      id: 10,
+      title: "HP Pavilion 15-DK1056WM",
+      "description": "HP Pavilion 15-DK1056WM Gaming...",
+      "price": [{ "ex": 1099 }],
+      "discountPercentage": 6.18,
+      "rating": 4.43,
+      "stock": 89
+    }, {
+      "id": 10,
+      "title": "HP Pavilion 15-DK1056WM",
+      "description": "HP Pavilion 15-DK1056WM Gaming...",
+      "price": [{ "ex": 1093 }],
+      "discountPercentage": 6.18,
+      "rating": 4.43,
+      "stock": 89
+    }, {
+      "id": 10,
+      "title": "HP Pavilion 15-DK1056WM",
+      "description": "HP Pavilion 15-DK1056WM Gaming...",
+      "price": [{ "ex": 10959 }],
+      "discountPercentage": 6.18,
+      "rating": 4.43,
+      "stock": 89
+    }, {
+      "id": 10,
+      "title": "HP Pavilion 15-DK1056WM",
+      "description": "HP Pavilion 15-DK1056WM Gaming...",
+      "price": [{ "ex": 1345 }],
+      "discountPercentage": 6.18,
+      "rating": 4.43,
+      "stock": 89
+    }, {
+      "id": 10,
+      "title": "HP Pavilion 15-DK1056WM",
+      "description": "HP Pavilion 15-DK1056WM Gaming...",
+      "price": [{ "ex": 15645 }],
+      "discountPercentage": 6.18,
+      "rating": 4.43,
+      "stock": 89
+    }, {
+      "id": 10,
+      "title": "HP Pavilion 15-DK1056WM",
+      "description": "HP Pavilion 15-DK1056WM Gaming...",
+      "price": [{ "ex": 1434 }],
+      "discountPercentage": 6.18,
+      "rating": 4.43,
+      "stock": 89
+    }, {
+      "id": 10,
+      "title": "HP Pavilion 15-DK1056WM",
+      "description": "HP Pavilion 15-DK1056WM Gaming...",
+      "price": [{ "ex": 1453 }],
+      "discountPercentage": 6.18,
+      "rating": 4.43,
+      "stock": 89
+    }, {
+      "id": 10,
+      "title": "HP Pavilion 15-DK1056WM",
+      "description": "HP Pavilion 15-DK1056WM Gaming...",
+      "price": [{ "ex": 1065 }, { "ex": 34534 }],
+      "discountPercentage": 6.18,
+      "rating": 4.43,
+      "stock": 89
+    }, {
+      "id": 10,
+      "title": "HP Pavilion 15-DK1056WM",
+      "description": "HP Pavilion 15-DK1056WM Gaming...",
+      "price": [{ "ex": 1076 }],
+      "discountPercentage": 6.18,
+      "rating": 4.43,
+      "stock": 89
+    }, {
+      "id": 10,
+      "title": "HP Pavilion 15-DK1056WM",
+      "description": "HP Pavilion 15-DK1056WM Gaming...",
+      "price": [{ "ex": 1011 }],
+      "discountPercentage": 6.18,
+      "rating": 4.43,
+      "stock": 89
+    }]
+    let totalSum: number = 0
+    const x = data.forEach((item: any) => (item.price.forEach((prices: any) => totalSum = totalSum + prices.ex)))
+    console.log(totalSum);
 
-  // const totalSum = data.reduce((sum, item) => {
-  //   // debugger
-  //   const prices = item.price.map((item) => item.ex);
-  //   return sum + prices.reduce((priceSum, price) => priceSum + price);
-  // },0);
-  // console.log(totalSum);
+    // const totalSum = data.reduce((sum, item) => {
+    //   // debugger
+    //   const prices = item.price.map((item) => item.ex);
+    //   return sum + prices.reduce((priceSum, price) => priceSum + price);
+    // },0);
+    // console.log(totalSum);
 
-  // let sum = 0;
-  // for(let i=0;i<data.length;i++) {
-  //   const prices = data[i].price.map((item) => item.ex);
-  //    sum + prices.reduce((priceSum, price) => priceSum + price);
-  // }
-}
+    // let sum = 0;
+    // for(let i=0;i<data.length;i++) {
+    //   const prices = data[i].price.map((item) => item.ex);
+    //    sum + prices.reduce((priceSum, price) => priceSum + price);
+    // }
+  }
 
-filterSix(){
- let  Actors = [
-    {
-      "name": "Tom Cruise",
-      "age": 56,
-      "Born At": "Syracuse, NY",
-      "Birthdate": "July 3, 1962",
-      "photo": "https://jsonformatter.org/img/tom-cruise.jpg",
-      "wife": null,
-      "weight": 67.5,
-      "hasChildren": true,
-      "hasGreyHair": false,
-      "children": [
-        "Suri",
-        "Isabella Jane",
-        "Connor"
-      ]
-    },
-    {
-      "name": "Robert Downey Jr.",
-      "age": 53,
-      "Born At": "New York City, NY",
-      "Birthdate": "April 4, 1965",
-      "photo": "https://jsonformatter.org/img/Robert-Downey-Jr.jpg",
-      "wife": "Susan Downey",
-      "weight": 77.1,
-      "hasChildren": true,
-      "hasGreyHair": false,
-      "children": [
-        "Indio Falconer",
-        "Avri Roel",
-        "Exton Elias"
-      ]
-    }
-  ]
-  let totalSum = 0;
-  const x = Actors.forEach((item:any) =>  totalSum = totalSum + item.age)
-  console.log(totalSum);
-}
-}
+  filterSix() {
+    let Actors = [
+      {
+        "name": "Tom Cruise",
+        "age": 56,
+        "Born At": "Syracuse, NY",
+        "Birthdate": "July 3, 1962",
+        "photo": "https://jsonformatter.org/img/tom-cruise.jpg",
+        "wife": null,
+        "weight": 67.5,
+        "hasChildren": true,
+        "hasGreyHair": false,
+        "children": [
+          "Suri",
+          "Isabella Jane",
+          "Connor"
+        ]
+      },
+      {
+        "name": "Robert Downey Jr.",
+        "age": 53,
+        "Born At": "New York City, NY",
+        "Birthdate": "April 4, 1965",
+        "photo": "https://jsonformatter.org/img/Robert-Downey-Jr.jpg",
+        "wife": "Susan Downey",
+        "weight": 77.1,
+        "hasChildren": true,
+        "hasGreyHair": false,
+        "children": [
+          "Indio Falconer",
+          "Avri Roel",
+          "Exton Elias"
+        ]
+      }
+    ]
+    let totalSum = 0;
+    const x = Actors.forEach((item: any) => totalSum = totalSum + item.age)
+    console.log(totalSum);
+  }
+
+  filterSeven() {
+    let array = [
+      {
+        "name": "Product A",
+        "price": 19.99,
+        "description": "A fantastic product",
+        "reviews": [
+          {
+            "user": "Customer 1",
+            "rating": 5,
+            "comment": "Great product!"
+          },
+          {
+            "user": "Customer 2",
+            "rating": 4,
+            "comment": "Not bad, but could be improved."
+          }
+        ]
+      },
+      {
+        "name": "Product B",
+        "price": 29.99,
+        "description": "Another great product",
+        "reviews": [
+          {
+            "user": "Customer 3",
+            "rating": 5,
+            "comment": "Amazing!"
+          },
+          {
+            "user": "Customer 4",
+            "rating": 3,
+            "comment": "Decent, but a bit pricey."
+          }
+        ]
+      },
+      {
+        "name": "Product A",
+        "price": 39.99,
+        "description": "A fantastic product",
+        "reviews": [
+          {
+            "user": "Customer 1",
+            "rating": 5,
+            "comment": "Great product!"
+          },
+          {
+            "user": "Customer 2",
+            "rating": 4,
+            "comment": "Not bad, but could be improved."
+          }
+        ]
+      },
+      {
+        "name": "Product A",
+        "price": 49.99,
+        "description": "A fantastic product",
+        "reviews": [
+          {
+            "user": "Customer 1",
+            "rating": 5,
+            "comment": "Great product!"
+          },
+          {
+            "user": "Customer 2",
+            "rating": 4,
+            "comment": "Not bad, but could be improved."
+          }
+        ]
+      },
+      {
+        "name": "Product A",
+        "price": 59.99,
+        "description": "A fantastic product",
+        "reviews": [
+          {
+            "user": "Customer 1",
+            "rating": 5,
+            "comment": "Great product!"
+          },
+          {
+            "user": "Customer 2",
+            "rating": 4,
+            "comment": "Not bad, but could be improved."
+          }
+        ]
+      },
+      {
+        "name": "Product A",
+        "price": 69.99,
+        "description": "A fantastic product",
+        "reviews": [
+          {
+            "user": "Customer 1",
+            "rating": 5,
+            "comment": "Great product!"
+          },
+          {
+            "user": "Customer 2",
+            "rating": 4,
+            "comment": "Not bad, but could be improved."
+          }
+        ]
+      },
+      {
+        "name": "Product A",
+        "price": 79.99,
+        "description": "A fantastic product",
+        "reviews": [
+          {
+            "user": "Customer 1",
+            "rating": 5,
+            "comment": "Great product!"
+          },
+          {
+            "user": "Customer 2",
+            "rating": 4,
+            "comment": "Not bad, but could be improved."
+          }
+        ]
+      },
+      {
+        "name": "Product A",
+        "price": 89.99,
+        "description": "A fantastic product",
+        "reviews": [
+          {
+            "user": "Customer 1",
+            "rating": 5,
+            "comment": "Great product!"
+          },
+          {
+            "user": "Customer 2",
+            "rating": 4,
+            "comment": "Not bad, but could be improved."
+          }
+        ]
+      },
+      {
+        "name": "Product A",
+        "price": 99.99,
+        "description": "A fantastic product",
+        "reviews": [
+          {
+            "user": "Customer 1",
+            "rating": 5,
+            "comment": "Great product!"
+          },
+          {
+            "user": "Customer 2",
+            "rating": 4,
+            "comment": "Not bad, but could be improved."
+          }
+        ]
+      },
+      {
+        "name": "Product A",
+        "price": 109.99,
+        "description": "A fantastic product",
+        "reviews": [
+          {
+            "user": "Customer 1",
+            "rating": 5,
+            "comment": "Great product!"
+          },
+          {
+            "user": "Customer 2",
+            "rating": 4,
+            "comment": "Not bad, but could be improved."
+          }
+        ]
+      },
+      {
+        "name": "Product A",
+        "price": 119.99,
+        "description": "A fantastic product",
+        "reviews": [
+          {
+            "user": "Customer 1",
+            "rating": 5,
+            "comment": "Great product!"
+          },
+          {
+            "user": "Customer 2",
+            "rating": 4,
+            "comment": "Not bad, but could be improved."
+          }
+        ]
+      },
+      {
+        "name": "Product A",
+        "price": 129.99,
+        "description": "A fantastic product",
+        "reviews": [
+          {
+            "user": "Customer 1",
+            "rating": 5,
+            "comment": "Great product!"
+          },
+          {
+            "user": "Customer 2",
+            "rating": 4,
+            "comment": "Not bad, but could be improved."
+          }
+        ]
+      },
+      {
+        "name": "Product A",
+        "price": 139.99,
+        "description": "A fantastic product",
+        "reviews": [
+          {
+            "user": "Customer 1",
+            "rating": 5,
+            "comment": "Great product!"
+          },
+          {
+            "user": "Customer 2",
+            "rating": 4,
+            "comment": "Not bad, but could be improved."
+          }
+        ]
+      },
+      {
+        "name": "Product A",
+        "price": 149.99,
+        "description": "A fantastic product",
+        "reviews": [
+          {
+            "user": "Customer 1",
+            "rating": 5,
+            "comment": "Great product!"
+          },
+          {
+            "user": "Customer 2",
+            "rating": 4,
+            "comment": "Not bad, but could be improved."
+          }
+        ]
+      },
+      {
+        "name": "Product A",
+        "price": 159.99,
+        "description": "A fantastic product",
+        "reviews": [
+          {
+            "user": "Customer 1",
+            "rating": 5,
+            "comment": "Great product!"
+          },
+          {
+            "user": "Customer 2",
+            "rating": 4,
+            "comment": "Not bad, but could be improved."
+          },
+          // {
+          //   "added" : 10
+          // }
+        ]
+      }
+    ]
+    let totalSum: number = 0
+    const x = array.forEach((item: any) => {
+      totalSum = totalSum + item.price
+      item.reviews.forEach((review: any) => {
+        totalSum = totalSum + review.rating
+      });
+    })
+    // const z = array.forEach((reviews: any) => totalSum = totalSum + reviews.rating)
+    console.log(totalSum);
+  }
+
+
+  filterEight(){
+    let array = [
+      {
+        "name": "Product A",
+        "price": 19.99,
+        "description": "A fantastic product",
+        "reviews": [
+          {
+            "user": "Customer 1",
+            "rating": 5,
+            "comment": "Great product!"
+          },
+          {
+            "user": "Customer 2",
+            "rating": 4,
+            "comment": "Not bad, but could be improved."
+          },
+          {
+            "user": "Customer 4",
+            "rating": 3,
+            "comment": "Decent, but a bit pricey."
+          }
+        ]
+      },
+      {
+        "name": "Product B",
+        "price": 29.99,
+        "description": "Another great product",
+        "reviews": [
+          {
+            "user": "Customer 3",
+            "rating": 5,
+            "comment": "Amazing!"
+          },
+          {
+            "user": "Customer 4",
+            "rating": 3,
+            "comment": "Decent, but a bit pricey."
+          },
+          {
+            "user": "Customer 4",
+            "rating": 3,
+            "comment": "Decent, but a bit pricey."
+          }
+        ]
+      },
+    ]
+    let totalSum: number = 0
+    const x = array.forEach((item: any) => {
+      debugger
+      totalSum = totalSum + item.price
+      item.reviews.forEach((review: any) => {
+        totalSum = totalSum + review.rating
+      });
+    })
+    // const z = array.forEach((reviews: any) => totalSum = totalSum + reviews.rating)
+    console.log(totalSum);
+  }
+  }
+
+
